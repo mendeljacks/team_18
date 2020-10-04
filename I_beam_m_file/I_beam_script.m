906db7bed52b62c18d3e617dc313a59ed20065ca
Alex Jarosz <alex.jarosz.96@gmail.com>
2020-10-04 14:53:54 +1100

    Tolerance 1.50/2.00 for TC2

--- I_beam_m_file/I_beam_script.m
+++ I_beam_m_file/I_beam_script.m
@@ -12,22 +12,22 @@
 clear all; close all; clc;
 sigma = 100; % Max stress not allowed to exceed
 F     = 100e3; % Force applied
-dist  = [1000, 2000, 3000, 4000, 100]; % Distance from force applied  
+dist  = [100, 1000, 2000, 3000, 4000]; % Distance from force applied  
 
 t = 10; % Thickness of I-beam
 h = 1:1:1000-2*t; % Height of I-beam 
 w = 1:1:1000-2*t; % width of I-beam 
 
-tol = 1.02; % tolerance for the system (how close we want our desired shear modulus to be to the limit)
+tol = 2.00; % tolerance for the system (how close we want our desired shear modulus to be to the limit)
 
 %% Calling the ibeamfunct function
 % Input desired distance in the function: i.e. dist(1), dist(2), etc
-[zShear, zInt, XSA, y, Ix, geom] = ibeamfunct(sigma, F, t, h, w, dist(5), tol);
+[zShear, zInt, XSA, y, Ix, geom] = ibeamfunct(sigma, F, t, h, w, dist(4), tol);
 
 %% Determining the ideal geometry for the given dist requirement
 [M,I] = max(geom(:,:));
 [row_h,col_w] = find(geom == max(M));
 
 % Input desired distance in the fprintf: i.e. dist(1), dist(2), etc
-fprintf(' height (h): %4.0f \n width (w): %4.0f \n distance (dist) from force applied: %4.0f \n', row_h, col_w, dist(5))
+fprintf(' height (h): %4.0f \n width (w): %4.0f \n distance (dist) from force applied: %4.0f \n', row_h, col_w, dist(4))
 
